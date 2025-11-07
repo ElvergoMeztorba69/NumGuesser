420da2f5b027b3e5f67b0fc6ed1760c877da1e37
proccesrhyme630 <elbigotess88@gmail.com>
2025-11-07 15:34:02 +0100

    Allah Akbar

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input1 = new Scanner(System.in);
+
+        int max=3, min=1;
+
+        int randomNum = (int)(Math.random() * (max - min + 1)) + min;
+
+        System.out.println("UN NOMBRE SECRET \nS'HA GENERAT.\nINTENTA ENDEVINAR \nEL NOMBRE.");
+
+        int inputNum = input1.nextInt();
+
+        System.out.println("EL NOMBRE \nQUE HAS INTRODUIT \nÉS " + inputNum + ".");
+
+        if(randomNum == inputNum){
+
+            System.out.println("\nENHORABONA.\nSEMBLA QUE HAS\nENCERTAT EL NOMBRE.");
+
+        }
+
+        else {
+
+            System.out.println("\n...\nQUE INTERESSANT.\n...\nEM SEMBLA,\nQUE NO HAS ENCERTAT\nEL NOMBRE.");
+
+            System.out.println("\n...\nTOT I AIXÒ.\n\nHE DECIDIT, DONAR-TE\nUNA SEGONA\nOPORTUNITAT.");
+
+            if(inputNum > randomNum) {
+
+                System.out.println("\nET DONARÉ\nUNA PISTA.\n\nEL TEU NOMBRE\nÉS MÉS PETIT\nQUE EL NOMBRE GENERAT.");
+
+            }
+
+            else{
+
+                System.out.println("\nET DONARÉ\nUNA PISTA.\n\nEL TEU NOMBRE\nÉS MÉS GRAN\nQUE EL NOMBRE GENERAT.");
+
+            }
+
+
+            System.out.println("\n...ARA.\nARA, INTENTA ENDEVINAR\n EL NOMBRE.");
+
+            int inputNum2 = input1.nextInt();
+
+            if(randomNum == inputNum2){
+
+                System.out.println("\nENHORABONA.\nSEMBLA QUE HAS\nENCERTAT EL NOMBRE.");
+
+            }
+
+            else{
+
+                System.out.println("\n...\nQUE INTERESSANT.\n...\nEM SEMBLA,\nQUE NO HAS ENCERTAT\nEL NOMBRE. \nMALA SORT.");
+
+            }
+
+        }
+
+        input1.close();
+
+    }
+}
